863f8da86ab5870429347d4fa60b8809c8d201f4
Seunghoon Park <parkseunghoon@gpa.ac.kr>
2015-11-03 13:19:44 +0900

    Update myArrays.java

--- myArrays.java
+++ myArrays.java
@@ -1,1 +1,15 @@
-
+public class myArrays {
+  public static void main(String args[]) {
+    char ch[] = new char[26];
+    int index = 0;
+    for(int x=65; x<91; x++) {
+      ch[index] = (char) x;
+      System.out.println(ch[index]);
+      index++;
+    }
+    for(int i=0; i<ch.length-1;i++) {
+      System.out.println(ch[i] + ", ");
+    }
+    System.out.println(ch[25]);
+  }
+}
